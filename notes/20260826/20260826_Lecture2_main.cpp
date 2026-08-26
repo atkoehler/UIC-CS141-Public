fd9c1cb23b4908803b9e53b953a7a1d1ed738c5f
Adam T Koehler, PhD <akoehler@uic.edu>
2026-08-26 14:29:19 -0500

    post lecture initial push

--- notes/20260826/20260826_Lecture2_main.cpp
+++ notes/20260826/20260826_Lecture2_main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+
 int main()
 {
+    int x = 0;
+    x = x+1; // ++x, x++   x+=1 
+    //short, long
 
+    // string l;
+
+    if (/* condition */)
+    {
+
+    }
+
+    // while, for, for-each, do-while
+    while (/* condition */)
+    {
+
+    }
+
+    // for(/*init*/, /*condition*/, /*update*/)
+    
     return 0;
 }
