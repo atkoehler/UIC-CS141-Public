a3e360afb57e951293327f9a77aae1c646c0a0ea
Adam T Koehler, PhD <akoehler@uic.edu>
2026-08-31 10:49:24 -0500

    udpated with comments

--- notes/20260826/20260826_Lecture2_main.cpp
+++ notes/20260826/20260826_Lecture2_main.cpp
@@ -1,26 +1,39 @@
-#include <iostream>
+/// @file 20260826_Lecture3_main.cpp
+/// @author Adam T Koehler, PhD
+/// @date August 26, 2026
+/// @brief Code and lecture notes from the live lecture. 
+///     Some code may require -std=c++23
 
+// Libraries to accomplish various tasks
+#include <iostream> // Input-Output Streams
+
+// Specifying a namespace allows us to presume we are working in a 
+// specific namespace and not have to use the scope resolution operator
+// and namespace name (e.g. std::) with everything we need from that 
+// namespace. However, it does make the code development less 
+// "portable" as we are restricting the namespace.
 using namespace std;
 
-
+// C++ will uses a main() function as its starting function
 int main()
 {
-    int x = 0;
-    x = x+1; // ++x, x++   x+=1 
+    // numeric types
+    // int x = 0;
+    // x = x+1; // ++x, x++   x+=1 
     //short, long
 
     // string l;
 
-    if (/* condition */)
-    {
+    // if (/* condition */)
+    // {
 
-    }
+    // }
 
     // while, for, for-each, do-while
-    while (/* condition */)
-    {
+    // while (/* condition */)
+    // {
 
-    }
+    // }
 
     // for(/*init*/, /*condition*/, /*update*/)
     
