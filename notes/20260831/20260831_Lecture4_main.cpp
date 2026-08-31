21cc497f49dc3816b542cbd3cf290c0456abb11a
Adam T Koehler, PhD <akoehler@uic.edu>
2026-08-31 11:04:22 -0500

    initial push of 8/31 lecture notes

---
+++ notes/20260831/20260831_Lecture4_main.cpp
@@ -0,0 +1,34 @@
+/// @file 20260831_Lecture4_main.cpp
+/// @author Adam T Koehler, PhD
+/// @date August 31, 2026
+/// @brief Code and lecture notes from the live lecture. 
+///     Some code may require -std=c++23
+
+
+// Libraries to accomplish various tasks
+#include <iostream> // Input-Output Streams
+#include <string>   // For the C++ string datatype
+#include <vector>   // For using vector containers for many counters
+
+
+using namespace std;
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// Stepwise development of a Frequency Counter program a pratical exploration  
+// of vectors, counting, strings, branches, and loops within C++.
+//
+// Pairs with Lecture 4 activities with string function development.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+/// @brief main function for running our examples
+/// @param argc the number of command line arguments
+/// @param argv the array containing c-strings of command line args
+int main(int argc, char *argv[])
+{
+ 
+    return 0;
+}
